3eb15f531c7358ce97409b4944b817b9ac041736
Dylan Gould <dylangood@gmail.com>
2017-02-24 15:49:36 -0800

    Implement _.map, which makes _.pluck also work
    I managed to make the _.map function work on my first try! I
    think I'm starting to understand the difference between parameters
    and arguments in a very real way, especially in function literals.

--- src/underbar.js
+++ src/underbar.js
@@ -135,18 +135,20 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+
+    var result = [];
+
+    _.each(collection, function(value) {
+      result.push(iterator(value));
+    });
+
+    return result;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
   };
 
-  /*
-   * TIP: map is really handy when you want to transform an array of
-   * values into a new array of values. _.pluck() is solved for you
-   * as an example of this.
-   */
-
-  // Takes an array of objects and returns and array of the values of
+  // Takes an array of objects and returns an array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
