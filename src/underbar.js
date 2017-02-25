c7b95b04b78f6d42c8e291cad5b4199ddf44e3b7
Dylan Gould <dylangood@gmail.com>
2017-02-24 16:24:00 -0800

    Partially solve _.reduce, but some tests fail
    The second use case, where you don't pass in an accumulator
    still fails, also some cases where you send in undefined data,
    which seems like I should filter away. The instructions don't
    actually say how undefined data should be treated, though.

--- src/underbar.js
+++ src/underbar.js
@@ -181,6 +181,9 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(value) {
+        accumulator = iterator(accumulator, value);     
+    });
   };
 
   // Determine if the array or object contains a given value (using `===`).
